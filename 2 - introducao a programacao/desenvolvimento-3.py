7457675e4de54d48b913af65a984ce0795a5cbd4
Débora de Oliveira Silva <deboraverzul@gmail.com>
2024-01-25 10:52:36 -0300

    resposta desenvolvimento 3

---
+++ 2 - introducao a programacao/desenvolvimento-3.py
@@ -0,0 +1,14 @@
+# ordem crescente
+for i in range(21):
+    if(i == 13):
+        continue
+    print(i)
+
+# ordem decrescente
+i = 20
+while (i >= 0):
+    if(i == 13):
+        i = i - 1
+        continue
+    print(i)
+    i = i - 1
